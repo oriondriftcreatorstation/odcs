79333131be3def50ff5966035ec3e9f72c2d6d64
Orion Drift Creator Station <oriondriftcreatorstation@gmail.com>
2026-09-02 01:28:37 +0800

    Update login.js

--- api/login.js
+++ api/login.js
@@ -2,7 +2,11 @@ module.exports = (req, res) => {
   const CLIENT_ID = process.env.DISCORD_CLIENT_ID;
   const REDIRECT_URI = process.env.DISCORD_REDIRECT_URI;
 
-  const url = `https://discord.com/api/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&response_type=code&scope=identify`;
+  // Get the page the user was on (from the query parameter)
+  const returnTo = req.query.return_to || '/upload.html';
+
+  // Build the Discord OAuth URL with the return URL stored in the 'state' parameter
+  const url = `https://discord.com/api/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&response_type=code&scope=identify&state=${encodeURIComponent(returnTo)}`;
 
   res.redirect(302, url);
 };
